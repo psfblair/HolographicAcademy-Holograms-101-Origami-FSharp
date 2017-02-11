1771c335637f473554cef493c2d38cef42bb7a34
paulblair <paulblair@SUPERNOVA>
2017-02-10 19:10:45 -0500

    Working version that eliminates two vals and an unnecessary property.

--- src/GazeGestureManager.fs
+++ src/GazeGestureManager.fs
@@ -6,48 +6,44 @@ open UnityEngine.VR.WSA.Input
 type GazeGestureManager() =
     inherit MonoBehaviour()
 
-    // All this rigmarole is needed because we need the initial value on the property to be null
-    // Static fields are not serialized in Unity
-    [<DefaultValue>] static val mutable private instance: GazeGestureManager
-    static member Instance = GazeGestureManager.instance
-    
-    // Represents the hologram that is currently being gazed at.
-    [<SerializeField>] [<DefaultValue>] val mutable focusedObject: GameObject
-    member this.FocusedObject = this.focusedObject
-    
-    [<SerializeField>] [<DefaultValue>] val mutable recognizer: GestureRecognizer
+    // Static fields are not serialized in Unity, but this will last as long as the class is loaded
+    static let mutable instance: Option<GazeGestureManager> = None
 
-    // Use this for initialization
+    // We need to serialize the recognizer. Otherwise when the manager is deserialized
+    // it will have a null recognizer
+    [<SerializeField>] [<DefaultValue>] val mutable recognizer: GestureRecognizer
+   
+    // Represents the hologram that is currently being gazed at.
+    let mutable maybeFocusedObject: Option<GameObject> = None
+
     member this.Start() =
-        GazeGestureManager.instance <- this
+        instance <- Some(this)
 
         // Set up a GestureRecognizer to detect Select gestures.
         this.recognizer <- new GestureRecognizer()
         this.recognizer.add_TappedEvent(
             fun source tapCount ray ->
                 // Send an OnSelect message to the focused object and its ancestors.
-                match this.FocusedObject with
-                    | null -> ()
-                    | _ -> this.FocusedObject.SendMessageUpwards("OnSelect")
+                match maybeFocusedObject with
+                    | Some(focusedObject) -> focusedObject.SendMessageUpwards("OnSelect")
+                    | None -> ()
             )
         this.recognizer.StartCapturingGestures()
 
     // Update is called once per frame
     member this.Update() =
-        // Figure out which hologram is focused this frame.
-        let oldFocusObject: GameObject = this.FocusedObject
-
         // Do a raycast into the world based on the user's head position and orientation.
         let headPosition = Camera.main.transform.position
         let gazeDirection = Camera.main.transform.forward
 
-        match Physics.Raycast(headPosition, gazeDirection) : (bool * RaycastHit) with
+        let maybeNewFocusedObject = match Physics.Raycast(headPosition, gazeDirection) : (bool * RaycastHit) with
             // If the raycast hit a hologram, use that as the focused object.
-            | true, hitInfo -> this.focusedObject <- hitInfo.collider.gameObject
+            | true, hitInfo -> hitInfo.collider.gameObject |> Some
             // If the raycast did not hit a hologram, clear the focused object. The outside world expects null, so we have to use it.
-            | _ -> this.focusedObject <- null
+            | _ -> None
 
         // If the focused object changed this frame, start detecting fresh gestures again.
-        if this.FocusedObject <> oldFocusObject then
+        if maybeNewFocusedObject <> maybeFocusedObject then
+            maybeFocusedObject <- maybeNewFocusedObject
             this.recognizer.CancelGestures()
             this.recognizer.StartCapturingGestures()
