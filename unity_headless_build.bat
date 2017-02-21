f19d403707b840d538509b482a9a679e83573f11
Paul Blair <pblair@example.com>
2017-02-21 16:55:01 -0500

    Remove extraneous quotation mark.

--- unity_headless_build.bat
+++ unity_headless_build.bat
@@ -1,4 +1,4 @@
 SET UNITY_FOLDER=Unity 5.6.0b9
-SET PROJECT_DIR="C:\Users\psfblair\Documents\holodevelop\MicrosoftAcademy\101-Origami""
+SET PROJECT_DIR="C:\Users\psfblair\Documents\holodevelop\MicrosoftAcademy\101-Origami"
 
 "C:\Program Files\%UNITY_FOLDER%\Editor\Unity.exe" -batchmode -quit -projectPath "%PROJECT_DIR%" -executeMethod Autobuild.Build
