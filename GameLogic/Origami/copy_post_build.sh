3da0f5e8f47dce713d8d9e1661313c7202bb7088
Paul Blair <pblair@example.com>
2017-03-09 21:23:31 -0500

    Again fix path in post-build copy script.

--- GameLogic/Origami/copy_post_build.sh
+++ GameLogic/Origami/copy_post_build.sh
@@ -9,7 +9,7 @@ LOCAL_PLUGINS_DIR="$MY_DIR/../../Assets/Plugins"
 cp -v "$TARGET_FILE" "$LOCAL_PLUGINS_DIR"
 cp -v "$TARGET_DIR/FSharp.Core.dll" "$LOCAL_PLUGINS_DIR" 
 
-REMOTE_PLUGINS_DIR="$MY_DIR/../../../../../remote/psfblair/Documents/holodevelop/MicrosoftAcademy/101-Origami/Origami/Assets/Plugins"
+REMOTE_PLUGINS_DIR="$MY_DIR/../../../../../remote/psfblair/Documents/holodevelop/MicrosoftAcademy/101-Origami/Assets/Plugins"
 
 if [ -d "$REMOTE_PLUGINS_DIR" ]
 then
